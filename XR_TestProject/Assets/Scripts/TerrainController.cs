aeb19bea9412bd4f65c33489de15fff942a510d7
Robbert Lengton <robbert.lengton@hotmail.nl>
2023-09-08 16:00:12 +0200

    Unity's Terrain works + terrain layer/texture

--- XR_TestProject/Assets/Scripts/TerrainController.cs
+++ XR_TestProject/Assets/Scripts/TerrainController.cs
@@ -5,7 +5,7 @@ public class TerrainController : MonoBehaviour
 {
     //[SerializeField] private MeshGenerator meshGenerator;
     [SerializeField] private Terrain terrainMesh;
-    [SerializeField] private Vector2Int terrainSize = new(1000, 1000);
+    [SerializeField] private int terrainSize = 1000;
 
     [Header("Flat area settings")]
     [SerializeField, Range(0f, 1f)] private float groundHeightPercent = 0.02f;
@@ -14,24 +14,49 @@ public class TerrainController : MonoBehaviour
     [Space(10)]
     [SerializeField] private PerlinNoise perlinNoise;
     [Space(10)]
-    [SerializeField] private Gradient gradient;
+    [SerializeField, Range(0f,1f)] private float[] terrainLayerHeightPos;
 
     private int xLength;
     private int zLength;
 
     public void GenerateTerrain()
     {
+        TerrainData terrainData = terrainMesh.terrainData;
         Vector3 flatAreaSettings = new(minBetweenPercent, groundHeightPercent, maxBetweenPercent);
-        //meshGenerator.Generate(terrainSize, flatAreaSettings, gradient, perlinNoise);
 
-        xLength = terrainSize.x;
-        zLength = terrainSize.y;
+        terrainData.heightmapResolution = terrainSize;
+        xLength = terrainData.heightmapResolution;
+        zLength = terrainData.heightmapResolution;
 
         float[,] heightmap = GetHeightmap(perlinNoise.GetHeightValues(xLength, zLength, flatAreaSettings));
 
-        terrainMesh.terrainData.size = new Vector3(xLength, perlinNoise.MaxPerlinNoiseValue, zLength);
-        terrainMesh.terrainData.heightmapResolution = xLength;
-        terrainMesh.terrainData.SetHeights(0, 0, heightmap);
+        terrainData.size = new Vector3(xLength, perlinNoise.MaxPerlinNoiseValue, zLength);
+        terrainData.alphamapResolution = xLength;
+        terrainData.SetHeights(0, 0, heightmap);
+
+        float[,,] alphaMap = terrainData.GetAlphamaps(0, 0, terrainData.alphamapWidth, terrainData.alphamapHeight);
+
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                for (int i = 0; i < terrainData.terrainLayers.Length; i++) alphaMap[x, y, i] = 0f;
+
+                float heightValue = heightmap[x, y];
+
+                for (int i = 0; i < terrainLayerHeightPos.Length; i++)
+                {
+                    if (terrainLayerHeightPos[i] >= heightValue)
+                    {
+                        alphaMap[x, y, i] = 1f;
+                        break;
+                    }
+                }
+            }
+        }
+
+        terrainData.SetAlphamaps(0, 0, alphaMap);
+        terrainMesh.Flush();
     }
 
     private float[,] GetHeightmap(float[,] heightValues)
